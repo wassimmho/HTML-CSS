4d02480104546eac43e87c1c8c7ff62c853ed268
wassimmho <wassim.mho000@gmail.com>
2025-02-24 18:46:19 -1200

    Starting with js and another project to start

---
+++ project12/pr12.js
@@ -0,0 +1,9 @@
+let x = 100;
+//console.log(x);
+//console.log("Hello World");
+
+console.log(`whats up ${x}`);
+
+document.getElementById("nigga2").innerHTML = "<h2>have to do always have always did</h2>";
+document.getElementById("nigga").innerHTML = "<h2>have to do always have always did</h2>";
+document.getElementById("nigga").innerText = "starting this shit";
